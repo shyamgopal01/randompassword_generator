450b421cb650ed1b7e70ef8ad5d9406248264928
shyamgopal01 <shyamgopalit@gmail.com>
2021-03-04 12:39:09 +0530

    msg

--- randompassword_generator.py
+++ randompassword_generator.py
@@ -1,38 +1,32 @@
 import random
 import array
 
-length = 12
 
-number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-             'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
-             'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-             'z']
+def randompwd_gen():
+    length = 12
+    number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-             'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q',
-             'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-             'Z']
+    lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                 'v', 'w', 'x', 'y', 'z']
 
-specialcharacters = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
-                     '*', '(', ')', '<']
+    uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q', 'R', 'S', 'T', 'U',
+                 'V', 'W', 'X', 'Y', 'Z']
 
-combination = number + lowercase + uppercase + specialcharacters
+    specialcharacters = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>', '*', '(', ')', '<']
 
-rand_num = random.choice(number)
-rand_upper = random.choice(uppercase)
-rand_lower = random.choice(lowercase)
-rand_spl = random.choice(specialcharacters)
+    combination = number + lowercase + uppercase + specialcharacters
 
-password = rand_num + rand_upper + rand_lower + rand_spl
+    rand_num = random.choice(number)
+    rand_upper = random.choice(uppercase)
+    rand_lower = random.choice(lowercase)
+    rand_spl = random.choice(specialcharacters)
 
-for x in range(length - 4):
-    password = password + random.choice(combination)
-    passwordlist = array.array('u', password)
-    random.shuffle(passwordlist)
-
-password = ""
-for x in passwordlist:
-    password = password + x
-
-print(password)
+    password = rand_num + rand_upper + rand_lower + rand_spl
+    for x in range(length - 4):
+        password = password + random.choice(combination)
+        passwordlist = array.array('u', password)
+        random.shuffle(passwordlist)
+    password = ""
+    for x in passwordlist:
+        password = password + x
+    return (password)
